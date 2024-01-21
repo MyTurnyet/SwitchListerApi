098d8acf6d2f20f289444a599209802c656ec0c3
Paige Watson <paige.watson@outlook.com>
2024-01-20 16:15:58 -0800

    Initial Commit

---
+++ src/main/java/com/softwareascraft/switchlisterapi/SwitchListerApiApplication.java
@@ -0,0 +1,13 @@
+package com.softwareascraft.switchlisterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchListerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitchListerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softwareascraft/switchlisterapi/SwitchListerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareascraft.switchlisterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchListerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
