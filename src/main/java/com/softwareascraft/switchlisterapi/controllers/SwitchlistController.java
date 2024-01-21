90d767a98f5bc791a9750e9a45134bdb29fee44f
Paige Watson <paige.watson@outlook.com>
2024-01-20 16:41:34 -0800

    use string builder to change train name

--- src/main/java/com/softwareascraft/switchlisterapi/controllers/SwitchlistController.java
+++ src/main/java/com/softwareascraft/switchlisterapi/controllers/SwitchlistController.java
@@ -1,14 +1,15 @@
 package com.softwareascraft.switchlisterapi.controllers;
 
 public class SwitchlistController {
-    public String getSwitchList() {
-        return "Train 1234-Local\n" +
-                "\n" +
-                "Town1\n" +
-                "---------\n" +
-                "\n" +
-                "* Grain Elevator *\n" +
-                "pickup\tBNSF 1234\n" +
-                "set out\tCPR 9876";
+    public String getSwitchListFor(String trainName) {
+        return new StringBuilder()
+                .append("Train ").append(trainName)
+                .append("\n").append("\n")
+                .append("Town1\n")
+                .append("---------\n").append("\n")
+                .append("* Grain Elevator *\n")
+                .append("pickup\tBNSF 1234\n")
+                .append("set out\tCPR 9876")
+                .toString();
     }
 }
--- src/test/java/com/softwareascraft/switchlisterapi/controllers/SwitchlistControllerTests.java
+++ src/test/java/com/softwareascraft/switchlisterapi/controllers/SwitchlistControllerTests.java
@@ -9,12 +9,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Tag("Unit")
 public class SwitchlistControllerTests {
     @Test
-    void should_returnSimpleSwitchlist() {
+    void should_returnSwitchlistForTrain1234_Local() {
         final SwitchlistController controller = new SwitchlistController();
-        final String switchList = controller.getSwitchList();
+        final String switchList = controller.getSwitchListFor("1234-Local");
 
         assertThat(switchList).isEqualTo(
                 "Train 1234-Local\n" +
+                "\n" +
+                "Town1\n" +
+                "---------\n" +
+                "\n" +
+                "* Grain Elevator *\n" +
+                "pickup\tBNSF 1234\n" +
+                "set out\tCPR 9876");
+    }
+    @Test
+    void should_returnSimpleSwitchlist() {
+        final SwitchlistController controller = new SwitchlistController();
+        final String switchList = controller.getSwitchListFor("2345-Express");
+
+        assertThat(switchList).isEqualTo(
+                "Train 2345-Express\n" +
                 "\n" +
                 "Town1\n" +
                 "---------\n" +
