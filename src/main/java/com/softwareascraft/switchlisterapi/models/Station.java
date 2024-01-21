db8434dbe57f04c547fa673c7b9a060d391950ba
Paige Watson <paige.watson@outlook.com>
2024-01-20 20:47:02 -0800

    add station class and return name

---
+++ src/main/java/com/softwareascraft/switchlisterapi/models/Station.java
@@ -0,0 +1,13 @@
+package com.softwareascraft.switchlisterapi.models;
+
+public class Station {
+    private final String name;
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public String Name() {
+        return name;
+    }
+}
---
+++ src/test/java/com/softwareascraft/switchlisterapi/models/StationTests.java
@@ -0,0 +1,20 @@
+package com.softwareascraft.switchlisterapi.models;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@Tag("Unit")
+public class StationTests {
+    @Test
+    void returnsStationName1() {
+        final Station station = new Station("Name1");
+        assertThat(station.Name()).isEqualTo("Name1");
+    }
+    void returnsStationName2() {
+        final Station station = new Station("Name2");
+        assertThat(station.Name()).isEqualTo("Name2");
+    }
+}
