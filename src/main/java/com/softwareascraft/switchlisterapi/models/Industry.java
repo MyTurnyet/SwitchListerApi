83b8c3343a8a462190720adbc4b3e2ccef93a02a
Paige Watson <paige.watson@outlook.com>
2024-01-22 11:06:39 -0800

    show cars needed as false when an industry has maxCars

--- src/main/java/com/softwareascraft/switchlisterapi/models/Industry.java
+++ src/main/java/com/softwareascraft/switchlisterapi/models/Industry.java
@@ -1,17 +1,21 @@
 package com.softwareascraft.switchlisterapi.models;
 
+import java.util.List;
+
 public class Industry {
 
     public final String name;
     private final int maxCars;
+    private final List<RollingStock> carsAtIndustry;
 
-    public Industry(String name, int maxCars) {
+    public Industry(String name, int maxCars, List<RollingStock> existingCarList) {
         this.name = name;
         this.maxCars = maxCars;
+        this.carsAtIndustry = existingCarList;
     }
 
 
     public boolean needsCars() {
-        return maxCars > 0;
+        return maxCars > carsAtIndustry.size();
     }
 }
--- src/test/java/com/softwareascraft/switchlisterapi/models/IndustryTests.java
+++ src/test/java/com/softwareascraft/switchlisterapi/models/IndustryTests.java
@@ -3,6 +3,9 @@ package com.softwareascraft.switchlisterapi.models;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -10,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class IndustryTests {
     @Test
     void shouldReturnName() {
-        final Industry industry = new Industry("Test Industry", 0);
+        final Industry industry = new Industry("Test Industry", 0, Collections.emptyList());
         final String name = industry.name;
         assertThat(name).isEqualTo("Test Industry");
     }
@@ -18,20 +21,21 @@ public class IndustryTests {
     @Test
     void doesNotNeedCars() {
         int maxCars = 0;
-        final Industry industry = new Industry("Test 1", maxCars);
+        final Industry industry = new Industry("Test 1", maxCars, Collections.emptyList());
         assertThat(industry.needsCars()).isFalse();
     }
 
     @Test
     void needsCarsWhenItHasNone() {
         int maxCars = 1;
-        final Industry industry = new Industry("Test 1", maxCars);
+        final Industry industry = new Industry("Test 1", maxCars, Collections.emptyList());
         assertThat(industry.needsCars()).isTrue();
     }
-//    @Test
-//    void DoesNotCarsWhenItHasOne() {
-//        int maxCars = 1;
-//        final Industry industry = new Industry("Test 1", maxCars);
-//        assertThat(industry.needsCars()).isTrue();
-//    }
+    @Test
+    void DoesNotCarsWhenItHasOne() {
+        int maxCars = 1;
+        final List<RollingStock> existingCarList = List.of(new RollingStock("BNSF", 1234));
+        final Industry industry = new Industry("Test 1", maxCars, existingCarList);
+        assertThat(industry.needsCars()).isFalse();
+    }
 }
