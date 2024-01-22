1f6ad5016337dc89acb4939baf5618607d5e1c46
Paige Watson <paige.watson@outlook.com>
2024-01-21 16:10:14 -0800

    return whether the station needs cars or not

--- src/main/java/com/softwareascraft/switchlisterapi/models/Station.java
+++ src/main/java/com/softwareascraft/switchlisterapi/models/Station.java
@@ -1,13 +1,23 @@
 package com.softwareascraft.switchlisterapi.models;
 
+import java.util.List;
+
 public class Station {
     private final String name;
+    private final int maxNumberOfCars;
+    private final List<String> currentCarsArray;
 
-    public Station(String name) {
+    public Station(String name, int maxNumberOfCars, List<String> currentCarsArray) {
         this.name = name;
+        this.maxNumberOfCars = maxNumberOfCars;
+        this.currentCarsArray = currentCarsArray;
     }
 
     public String Name() {
         return name;
+    }
+
+    public boolean needsCars() {
+        return currentCarsArray.size() < maxNumberOfCars;
     }
 }
--- src/test/java/com/softwareascraft/switchlisterapi/models/StationTests.java
+++ src/test/java/com/softwareascraft/switchlisterapi/models/StationTests.java
@@ -3,6 +3,9 @@ package com.softwareascraft.switchlisterapi.models;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -10,13 +13,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StationTests {
     @Test
     void returnsStationName1() {
-        final Station station = new Station("Name1");
+        final Station station = new Station("Name1", 0, Collections.emptyList());
         assertThat(station.Name()).isEqualTo("Name1");
     }
 
     @Test
     void returnsStationName2() {
-        final Station station = new Station("Name2");
+        final Station station = new Station("Name2", 0, Collections.emptyList());
         assertThat(station.Name()).isEqualTo("Name2");
+    }
+    @Test
+    void needsCars() {
+        int maxNumberOfCars = 1;
+        final List<String> currentCarsArray = Collections.emptyList();
+        final Station station = new Station("Name1",maxNumberOfCars, currentCarsArray);
+        final boolean needsCars = station.needsCars();
+        assertThat(needsCars).isTrue();
+    }
+    @Test
+    void doesNotNeedCars() {
+        final List<String> currentCarsArray = List.of("BNSF 1234");
+        final Station station = new Station("Name1", 1, currentCarsArray);
+        final boolean needsCars = station.needsCars();
+        assertThat(needsCars).isFalse();
     }
 }
